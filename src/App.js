3bc152de2ca220a3cbb779c559166eb0bb6028f3
Temitope <ojotemitopeidnoble10@gmail.com>
2021-10-08 10:05:49 -0700

    aaa

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import JokeList from './components/jokes/jokesList'
-import './app.css'
+import '../src/app.css'
 export default function App() {
   return (
    <div className='app'>
