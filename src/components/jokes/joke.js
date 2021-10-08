4f0a17f6b3a29c7380dec37583b1f557e7a058b0
Temitope <ojotemitopeidnoble10@gmail.com>
2021-10-08 09:49:54 -0700

    first commit

---
+++ src/components/jokes/joke.js
@@ -0,0 +1,89 @@
+import React,{useCallback} from 'react'
+import { Box, Typography } from "@mui/material";
+import ArrowUpwardOutlinedIcon from "@mui/icons-material/ArrowUpwardOutlined";
+import ArrowDownwardOutlinedIcon from "@mui/icons-material/ArrowDownwardOutlined";
+export default function Joke(props) {
+
+const { votes, text, upvote, downvote } = props;
+
+ const getEmoji = useCallback((votes) => {
+   if (votes >= 9) {
+     return "em em-rolling_on_the_floor_laughing";
+   } else if (votes >= 6) {
+     return "em em-laughing";
+   } else if (votes >= 3) {
+     return "em em-slightly_smiling_face";
+   } else if (votes >= 0) {
+     return "em em-neutral_face";
+   } else {
+     return "em em-angry";
+   }
+ }, []);
+      return (
+        <Box
+          style={{
+            display: "flex",
+            borderBottom: "2px solid #eeeeeeee",
+            alignItems: "center",
+            justifyContent: "center",
+            fontWeight: 400,
+          }}
+        >
+          <Box
+            style={{
+              display: "flex",
+              marginRight: "1rem",
+              justifyContent: "center",
+              // backgroundColor: "red",
+              alignItems: "center",
+            }}
+            sx={{
+              width: { lg: "20%", xs: "10%", sm: "10%" },
+              marginLeft: { xs: 4 },
+            }}
+          >
+            <ArrowUpwardOutlinedIcon
+              style={{ fontSize: "2rem", margin: 10, cursor: "pointer" }}
+              onClick={() => {
+                upvote();
+              }}
+            />
+
+            <Typography style={{ fontSize: 20 }}>{votes}</Typography>
+            <ArrowDownwardOutlinedIcon
+              style={{ fontSize: "2rem", margin: 10, cursor: "pointer" }}
+              onClick={() => {
+                downvote();
+              }}
+            />
+          </Box>
+          <Box
+            style={{ fontSize: "1.2rem", textOverflow: "wrap" }}
+            sx={{
+              width: { lg: "70%", xs: "80%", sm: "80%" },
+              marginLeft: { xs: 3 },
+            }}
+          >
+            {text}
+          </Box>
+          <Box
+            sx={{
+              width: { lg: "10%", xs: "10%", sm: "10%" },
+              marginLeft: { xs: 2 },
+            }}
+          >
+            <i
+              className={getEmoji(votes)}
+              style={{
+                fontSize: "3rem",
+                borderRadius: "50%",
+                marginLeft: "auto",
+                boxShadow:
+                  "0 19px 38px rgba(0, 0, 0, 0.3) ,0 15px 12px rgba(0, 0, 0, 0.1)",
+              }}
+            />
+          </Box>
+        </Box>
+      );
+ 
+}
---
+++ src/components/jokes/jokesList.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useState , useCallback} from "react";
+import { Box, Typography } from "@mui/material";
+import Joke from "./joke";
+import axios from "axios";
+
+export default function JokesList() {
+  
+const [jokes, setJokes] = useState(null);
+async function getJoke() {
+  let newJokes = [];
+  for (var i = 1; i < 7; i++) {
+    let res = await axios.get("https://icanhazdadjoke.com/", {
+      headers: { Accept: "application/json" },
+    });
+    newJokes.push({ id: i, text: res.data.joke, votes: 0 });
+  }
+  setJokes(newJokes);
+}
+
+useEffect(() => {
+  getJoke();
+}, []);
+
+  const handleVote = useCallback(
+    (id, offset) => {
+      let filteredJokes = jokes.filter((joke) => joke.id !== id);
+      let joke = jokes.find((joke) => joke.id === id);
+      joke.votes += offset;
+      filteredJokes.push(joke);
+      filteredJokes.sort((a, b) => b.votes - a.votes);
+      setJokes(filteredJokes);
+    },
+    [jokes, setJokes]
+  );
+
+ if(jokes){
+
+      return (
+        <Box
+          style={{ display: "flex" }}
+          sx={{
+            width: { lg: "80%", sm: "90%", xs: "90%" },
+            height: { lg: "80", sm: "90%", xs: "90%" },
+          }}
+        >
+          <Box
+            style={{
+              backgroundColor: "#9575cd",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              textAlign: "center",
+              boxShadow:
+                "0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.1)",
+              zIndex: 2,
+              borderRadius: 7,
+            }}
+            sx={{ width: { lg: "30%", sm: "20%", xs: "20%" } }}
+          >
+            <Typography
+              style={{
+                color: "#fff",
+                fontWeight: 700,
+                margin: 60,
+                letterSpacing: 0,
+              }}
+              sx={{ fontSize: { lg: "3rem", sm: "1.5rem" } }}
+            >
+              Dad
+              <br />
+              Jokes
+            </Typography>
+            <img
+              src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+              alt="im"
+              style={{
+                width: "50%",
+                boxShadow:
+                  "0 19px 38px rgba(0, 0, 0, 0.3) ,0 15px 12px rgba(0, 0, 0, 0.1)",
+                borderRadius: "50%",
+              }}
+            />
+          </Box>
+          <Box
+            style={{
+              height: "90%",
+              background: "#fff",
+
+              alignSelf: "center",
+              boxShadow:
+                "0 19px 38px rgba(0, 0, 0, 0.3) ,0 15px 12px rgba(0, 0, 0, 0.1)",
+              borderTopRightRadius: 7,
+              borderBottomRightRadius: 7,
+              overflow: "scroll",
+            }}
+            sx={{ width: { lg: "70%", xs: "90%", sm: "90%" } }}
+          >
+            {jokes.map((joke) => {
+              return (
+                <Joke
+                  votes={joke.votes}
+                  text={joke.text}
+                  upvote={() => {
+                    handleVote(joke.id, 1);
+                  }}
+                  downvote={() => {
+                    handleVote(joke.id, -1);
+                  }}
+                  key={joke.id}
+                />
+              );
+            })}
+          </Box>
+        </Box>
+      );
+   
+ }
+ else{
+   return(
+     <h1>loading...</h1>
+   )
+ }
+}
